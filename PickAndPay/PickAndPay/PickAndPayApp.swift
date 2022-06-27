d9038b832d26e52322667ffd80adbdf577573675
James-Suresh <dev.jamessuresh@gmail.com>
2022-06-27 15:41:43 -0400

    test commit

--- PickAndPay/PickAndPay/PickAndPayApp.swift
+++ PickAndPay/PickAndPay/PickAndPayApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct PickAndPayApp: App {
     let persistenceController = PersistenceController.shared
-
+    //hello
     var body: some Scene {
         WindowGroup {
             ContentView()
