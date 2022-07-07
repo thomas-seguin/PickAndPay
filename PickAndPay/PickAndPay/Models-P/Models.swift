603b4f1b079d4970616069d585000cf67f65ea8d
philip713 <105677806+philip713@users.noreply.github.com>
2022-07-07 14:42:30 -0600

    BrowseHistory and update on searchHistory
    Added a new table BrowseHistory for recording items the user clicked / browsed
    Completed Model, and db helper for it
    Also updated SearchHistory to return search entries  by user's most searched
    *Browse History is for when user actually clicked and checked a products details
    *Search History contains search phrases entered in the search bar

--- PickAndPay/PickAndPay/Models-P/Models.swift
+++ PickAndPay/PickAndPay/Models-P/Models.swift
@@ -127,7 +127,7 @@ struct WishListItem{
         
     }
 }
-
+//For Search Bar
 struct SearchHistoryItem{
     var searchHistoryId = 0 // assigned by database on creation
     var searchPhrase = ""
@@ -143,7 +143,27 @@ struct SearchHistoryItem{
         self.userId = userId
     }
 }
-
+//For User Browsing History (when user clicks item)
+struct BrowseHistoryItem{
+    var browseHistoryId = 0 // assigned by database on creation
+    var dateTime = ""
+    var productId = 0
+    var userId = ""
+    var browsedProduct : Product{
+        get{
+            return DBHelper.dbHelper.getProduct(productId: productId)
+        }
+    }
+    init(){
+        
+    }
+    init(id : Int, dateTime : String, productId : Int, userId : String){
+        self.browseHistoryId = id
+        self.dateTime = dateTime
+        self.productId = productId
+        self.userId = userId
+    }
+}
 struct Review{
     var reviewId = 0 // assigned by databse on creation
     var body = ""
