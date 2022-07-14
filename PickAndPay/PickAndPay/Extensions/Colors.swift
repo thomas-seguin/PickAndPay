26c522c964c85876e646e27c1cbaa81963f46167
thomas-seguin <seguinthomas7@gmail.com>
2022-07-14 16:14:33 -0400

    added color extension

---
+++ PickAndPay/PickAndPay/Extensions/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  PickAndPay
+//
+//  Created by admin on 7/14/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("background")
+    static let text = Color("text")
+    static let button = Color("button")
+}
