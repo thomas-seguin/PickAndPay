c988bc915d7601560aa2835abb66309e97ad085e
Zahid <9085730+ZanyZeee@users.noreply.github.com>
2022-07-19 16:46:01 -0400

    added MainTabView again

---
+++ PickAndPay/PickAndPay/Views/MainTabView.swift
@@ -0,0 +1,53 @@
+//
+//  TabView.swift
+//  PickAndPay
+//
+//  Created by admin on 7/17/22.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @StateObject private var tabViewModel = TabViewModel()
+    @ObservedObject var cartManager = CartManager()
+    @EnvironmentObject var authentication: Authentication
+    var body: some View {
+        NavigationView{
+        TabView{
+            CartView()
+                .tabItem {
+                    Label("Cart", systemImage: "cart")
+                    .badge(cartManager.products.count)
+
+                }
+            if authentication.isValidated{
+            AccountMenuView()
+                .tabItem {
+                    Label("Account", systemImage: "person.fill")
+        }
+            }
+            homeView()
+                .tabItem{
+                    Label("Home", systemImage: "house.fill")
+                }
+
+    }.environmentObject(cartManager )
+    .environmentObject(authentication)
+    .accentColor(.red)
+    .foregroundColor(/@START_MENU_TOKEN@/.blue/@END_MENU_TOKEN@/)
+
+
+    }
+        .onAppear{
+            if tabViewModel.checkRemember(){
+                authentication.isValidated = true
+            }
+        }
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
