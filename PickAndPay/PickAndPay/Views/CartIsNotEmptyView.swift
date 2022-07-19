9c69c29483f6adbbe734351f0168974e346004a6
Zahid <9085730+ZanyZeee@users.noreply.github.com>
2022-07-19 17:32:32 -0400

    changes to CartNotEmptyView

--- PickAndPay/PickAndPay/Views/CartIsNotEmptyView.swift
+++ PickAndPay/PickAndPay/Views/CartIsNotEmptyView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct CartIsNotEmptyView: View {
     @EnvironmentObject var cartManager : CartManager
+    @State var showAlert = false
     var body: some View {
         ScrollView {
             Text("My Cart").bold().font(.largeTitle)
@@ -19,21 +20,33 @@ struct CartIsNotEmptyView: View {
                 .background(.gray, in: RoundedRectangle(cornerRadius: 10))
                 
                 HStack {
+                    NavigationLink(destination: AddCardView()){
                     Text("Payment Options")
                         .frame(width: UIScreen.main.bounds.width, height: 60, alignment: .center)
                         .background(.yellow)
                     .cornerRadius(10)
                 }.background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 10))
+                    Button{
+        
+                        showAlert = true
+                        
+                    }label:{Text("Place Order")}
+                    
+                        .alert("Order Placed", isPresented: $showAlert) {
+                        Button("OK", role: .cancel) { }
+            }
+                
             }
             
         }
 
     }
-    func loadList(){}
+  
 }
 
 struct CartIsNotEmptyView_Previews: PreviewProvider {
     static var previews: some View {
         CartIsNotEmptyView()
     }
+}
 }
