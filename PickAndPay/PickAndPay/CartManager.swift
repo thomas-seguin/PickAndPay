6a71edb757c62e6f5ed3c8446f3bf547ed2a633a
weblearner21 <kuanypeter@gmail.com>
2022-07-19 21:22:43 -0400

    last update cart manager delete, add, and remove functions

--- PickAndPay/PickAndPay/CartManager.swift
+++ PickAndPay/PickAndPay/CartManager.swift
@@ -28,15 +28,13 @@ class CartManager: ObservableObject {
         DBHelper.dbHelper.insertToCart(username: UserSingleton.userData.currentUsername as NSString, productId: 2, qty: 1)
 
         items = DBHelper.dbHelper.getUserCart(username: UserSingleton.userData.currentUsername as NSString)
-        updateItems()
+        initialCost()
     }
 
     func addToCart(product: CartItem){
         //products.append(product)
-        
         total = total + product.cartProduct.price
         var amount = product.quantity + 1
-        print(amount)
         DBHelper.dbHelper.updateCartItemQty(qty: amount, itemCartId: product.itemCartId)
         updateItems()
 
@@ -46,26 +44,37 @@ class CartManager: ObservableObject {
         if product.quantity > 0 {
             total = total - product.cartProduct.price
             var amount = product.quantity - 1
-            print(amount)
             DBHelper.dbHelper.updateCartItemQty(qty: amount, itemCartId: product.itemCartId)
             updateItems()
+            
+            if amount == 0 {
+                deleteFromCart(product: product)
+            }
 
 
         }
+
+
     }
     
     func deleteFromCart(product: CartItem){
-        //items = items.filter{$0.productId != product.productId}
+        if product.quantity >= 1 {
+            total = total - (product.cartProduct.price * Double(product.quantity))
+        }
         DBHelper.dbHelper.removeFromCart(itemCartId: product.itemCartId)
         updateItems()
     }
     func updateItems(){
-        var count = 0
         items = DBHelper.dbHelper.getUserCart(username: UserSingleton.userData.currentUsername as NSString)
+
+    }
+    func initialCost(){
+        var count = 0
         while (items.count > count && items.count != 0){
             total += items[count].cartProduct.price
             count += 1
         }
 
+        
     }
 }
